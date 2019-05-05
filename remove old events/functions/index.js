2ded3a019c0a11918e652e6352bf18b7319d7b10
Matatov1989 <Matatov1989@gmail.com>
2019-05-05 19:21:31 +0300

    5.05.2019

--- remove old events/functions/index.js
+++ remove old events/functions/index.js
@@ -2,41 +2,35 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
+const bucket = admin.storage().bucket("gs://movere-point.appspot.com/");
 var db;
-//var count = 0;
 
-// Cut off time. Child nodes older than this will be deleted.
-const CUT_OFF_TIME = 2 * 60 * 60 * 1000; // 2 Hours in milliseconds.
-
-/**
- * This database triggered function will check for child nodes that are older than the
- * cut-off time. Each child needs to have a `timestamp` attribute.
- */
 exports.removeOldEvents = functions.https.onRequest((req, res) => {
-   db = admin.firestore();
-   db.collection("Events")
-     .where("eventStop", "<", new Date())
-     .get()
-     .then(querySnapshot => {
-       var promises = [];
-       querySnapshot.forEach(doc => {
-         console.log(doc.id);
-         promises.push(
-           db
-             .collection("Events")
-             .doc(doc.id)
-             .delete()
-         );
-       });
-       return Promise.all(promises);
-     })
-     .then(() => {
-       console.log('Successful');
-       res.send('Successful');
-     })
-     .catch(error => {
-       console.log('Error ', error);
-       res.status(500).send('Error ', error);
-     });
+  db = admin.firestore();
+  db.collection("Events")
+  .where("eventStop", "<", new Date())
+  .get()
+  .then(querySnapshot => {
+    var promises = [];
+
+    querySnapshot.forEach(doc => {
+      console.log('Event id = ', doc.id);
+      var strLen = doc.data().eventPhoto.length;
+      if (strLen > 0){
+        const file = bucket.file("Events/" + doc.id);
+        promises.push(file.delete());
+      }
+      promises.push(db.collection("Events").doc(doc.id).delete());
+    });
+    return Promise.all(promises);
+    })
+    .then(() => {
+      console.log('Successful');
+      res.send('Successful');
+    })
+    .catch(error => {
+      console.log('Error ', error);
+      res.status(500).send('Error ', error);
+    });
  });
